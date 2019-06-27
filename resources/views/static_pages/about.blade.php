4791b0bd5cfd9f2d043606338a06f102c7d961fc
zxl5802316 <1024659300@qq.com>
2019-06-27 12:13:29 +0800

    基础页面构建

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title','关于我')
+@section('content')
+
+    <h1>关于我</h1>
+
+@endsection
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title','帮助')
+@section('content')
+
+    <h1>帮助</h1>
+
+@endsection
