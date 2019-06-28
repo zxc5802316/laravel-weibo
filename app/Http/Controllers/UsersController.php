6b5fbfb4f9c85f31068ffa3336fb935f9b1406b2
zxl5802316 <1024659300@qq.com>
2019-06-28 15:41:51 +0800

    中文语言包

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,11 +7,27 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /**用户注册
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create(){
         return view('users.create');
     }
 
+    /**个人信息
+     * @param User $user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'=>'required|min:3|max:20',
+            'email'=>'required|email|unique:users,email|max:200',
+            'password'=>'required|min:6|confirmed'
+        ]);
     }
 }
