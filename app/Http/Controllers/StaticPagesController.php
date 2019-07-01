2de2ebf7660bf85be38df30e270799701d985ff3
zxl5802316 <1024659300@qq.com>
2019-07-01 14:56:14 +0800

    11

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,6 +8,7 @@ class StaticPagesController extends Controller
 {
     public function home()
     {
+        
         $feed_items = [];
         if (\Auth::check()){
             $feed_items = \Auth::user()->feed()->paginate(10);
